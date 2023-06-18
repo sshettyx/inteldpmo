54b14720a3273cd40081860ce03c0787c0344b91
sshettyx <shashirx.shetty@intel.com>
2023-06-18 13:25:22 +0530

    Add files via upload

--- local_dpmo_logs_collecter.py
+++ local_dpmo_logs_collecter.py
@@ -10,9 +10,9 @@ def coldboot():
 	        readline=file.read().splitlines()
         readline = (readline[0])
         global file_name
-        os.system(f"mkdir log_dpmo && cp -rf {readline} log_dpmo")
+        os.system(f"mkdir coldboot_log && cp -rf {readline} coldboot_log")
         file_name = str(input("Enter the name for the zip file you want to create(without extention): "))
-        os.system(f"zip -r {file_name}.zip log_dpmo")
+        os.system(f"zip -r {file_name}.zip coldboot_log")
         print ("\n\n")
     except:
         print(f"Unable to Read the directory")
@@ -26,9 +26,9 @@ def warmboot():
             readline=file.read().splitlines()    
         readline = (readline[0])
         global file_name
-        os.system(f"mkdir log_dpmo && cp -rf {readline} log_dpmo")
+        os.system(f"mkdir warmboot_logs && cp -rf {readline} warmboot_logs")
         file_name = str(input("Enter the name for the zip file you want to create (Without extention): "))
-        os.system(f"zip -r {file_name}.zip log_dpmo")
+        os.system(f"zip -r {file_name}.zip warmboot_logs")
         print ("\n\n")
         print ("Log has been collected")
     except:
@@ -43,23 +43,42 @@ def s0ix():
             readline=file.read().splitlines()
         readline = (readline[0])
         global file_name
-        os.system(f"mkdir log_dpmo && cp -rf {readline} log_dpmo")
+        os.system(f"mkdir S0ix_logs && cp -rf {readline} S0ix_logs")
         file_name = str(input("Enter the name for the zip file you want to create(Without extention): "))
-        os.system(f"zip -r {file_name}.zip log_dpmo")
+        os.system(f"zip -r {file_name}.zip S0ix_logs")
 
         print ("\n\n")
     except:
         print(f"Unable to Read the directory")
 
+def s0ix_pmgraph():
+    try:
+        print ("\nChecking the most recent s0ix_pmgraph iteration.\n\n")
+        os.system("ls -td /home/intel/pm-graph/suspend-* | head -n 1 > /home/intel/count.txt")
+        with open("/home/intel/count.txt", "r") as file:
+            readline=file.read().splitlines()        
+        readline = (readline[0])
+        global file_name
+        os.system(f"mkdir S0ix_logs_pmgraph && cp -rf {readline} S0ix_logs_pmgraph")
+        file_name = str(input("Enter the name for the zip file you want to create(Without extention): "))
+        os.system(f"zip -r {file_name}.zip S0ix_logs_pmgraph")
+
+        print ("\n\n")
+    except:
+        print(f"Unable to Read the directory")
+
+
 def main():
     
-    choice=str(input("\nPress 1 for Cold boot \nPress 2 for Warm boot \nPress 3 for s0ix\nEnter the test you want to check the cycles number for:"))
+    choice=str(input("\nPress 1 for Cold boot \nPress 2 for Warm boot \nPress 3 for s0ix(rtcwake) \nPress 4 for S0ix_pmgraph \nEnter the test you want to check the cycles number for:"))
     if choice=='1':
         coldboot()
     elif choice=='2':
         warmboot()
     elif choice=='3':
         s0ix()
+    elif choice=='4':
+        s0ix_pmgraph()
     else:
         print("Wrong Choice.Please Enter the valid Choice")
 
@@ -71,8 +90,8 @@ def debug():
         with open("/home/intel/dbg.txt", "r") as file:
             dbg=file.read().splitlines()
         dbg = (dbg[0])
-        os.system(f"mkdir log_debug && cp -rf {dbg} log_debug")
-        os.system(f"zip -u {file_name}.zip log_debug/*")
+        os.system(f"mkdir debug_logs && cp -rf {dbg} debug_logs")
+        os.system(f"zip -u {file_name}.zip debug_logs/*")
     except:
         print(f"Unable to Read the file")
 
@@ -87,6 +106,6 @@ def dg():
 
 main()
 dg()
-os.system("rm -rf log_debug log_dpmo")
+os.system("rm -rf debug_logs log_dpmo coldboot_log warmboot_logs S0ix_logs S0ix_logs_pmgraph")
 
 
