b277dd0590de566fa606e330748354b8a627a8c7
sshettyx <shashirx.shetty@intel.com>
2023-10-14 17:36:25 +0530

    Enable pstore logs
    Enable pstore logs in /etc/default grub file and updates the grub to collect the pstore logs in dpmo.

---
+++ add_pstore_debug.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Check if the script is running as root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as the root user."
+    exit 1
+fi
+
+sed -i "s/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"printk.always_kmsg_dump=y /g" /etc/default/grub
+
+# Update GRUB
+echo "Running: sudo update-grub"
+sudo update-grub
+
+# Prompt the user to reboot
+echo "Please reboot your system for the changes to take effect."
